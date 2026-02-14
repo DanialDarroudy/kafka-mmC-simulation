b41e9d5ad7eff4a1eb7e7b9e49e82078fdff50aa
DanialDarroudy <d.darroudi@mohaymen.ir>
2026-02-14 20:05:11 +0330

    implement MessageProcessor.

--- consumer/src/main/java/ir/sharif/consumer/module/message/process/business/MessageProcessor.java
+++ consumer/src/main/java/ir/sharif/consumer/module/message/process/business/MessageProcessor.java
@@ -2,15 +2,43 @@ package ir.sharif.consumer.module.message.process.business;
 
 import ir.sharif.consumer.module.message.process.abstraction.IMessageProcessor;
 import ir.sharif.consumer.module.message.process.dto.ProcessMessageRequestDto;
+import ir.sharif.consumer.module.metric.service.abstraction.IMetricService;
+import ir.sharif.consumer.module.metric.service.dto.MetricServiceRecord;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class MessageProcessor implements IMessageProcessor {
+    private final IMetricService metricService;
+
+    @Value("${simulation.service-mean-ms}")
+    private long meanServiceTime;
 
     @Override
     public void process(ProcessMessageRequestDto message) {
+        var serverId = Thread.currentThread().getName();
+        var dequeueTime = System.currentTimeMillis();
+        var waitingTime = dequeueTime - message.getArrivalTime();
+        var serviceTime = generateExponential(meanServiceTime);
+        var busyStart = System.currentTimeMillis();
 
+        try {
+            Thread.sleep(serviceTime);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        var finishTime = System.currentTimeMillis();
+        var responseTime = finishTime - message.getArrivalTime();
+        var busyDuration = finishTime - busyStart;
+
+        var metricRecord = new MetricServiceRecord(serverId, waitingTime, responseTime, serviceTime, busyDuration);
+        metricService.record(metricRecord);
+    }
+
+    private long generateExponential(long mean) {
+        return (long) (-mean * Math.log(1 - Math.random()));
     }
 }
